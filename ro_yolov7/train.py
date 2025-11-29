f62ef3511d5c1bbf7b08d3b87eeba4a34b612e3b
john <john@ramonaoptics.com>
2025-11-29 12:24:45 -0500

    set defaults

--- ro_yolov7/train.py
+++ ro_yolov7/train.py
@@ -982,16 +982,11 @@ def main():
         default="yolov7-tiny.yaml",
         help="model.yaml path"
     )
-    parser.add_argument(
-        "--data",
-        type=str,
-        default="hyp.scratch.tiny.yaml",
-        help="data.yaml path"
-    )
+    parser.add_argument("--data", type=str, default="coco.yaml", help="data.yaml path")
     parser.add_argument(
         "--hyp",
         type=str,
-        default="hyp.scratch.p5.yaml",
+        default="hyp.scratch.tiny.yaml",
         help="hyperparameters path",
     )
     parser.add_argument("--epochs", type=int, default=300)
